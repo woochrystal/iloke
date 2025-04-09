e5bfd555aca9dcf3019e1584744516b00e6282b4
DESKTOP-H208UA1\82104 <woochryfront@gmail.com>
2025-04-09 21:13:25 +0900

    250409 이로케 호스팅

--- admin/backend/utils/dbUtil.js
+++ admin/backend/utils/dbUtil.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 const db = require('mysql2');
 
 // 데이터베이스 연결 객체 생성
 const conn = db.createPool({
-    host: 'localhost',
-    user: 'iloke',
-    password: '1q2w3e4r!@#',
-    database: 'iloke'
+    // host: 'localhost',
+    // user: 'iloke',
+    // password: '1q2w3e4r!@#',
+    // database: 'iloke'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 // 서버 종료 시 DB 연결 종료
