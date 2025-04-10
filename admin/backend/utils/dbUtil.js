353ce97af14eda452318fb6d82838914d84c8ac7
DESKTOP-H208UA1\82104 <woochryfront@gmail.com>
2025-04-10 23:33:44 +0900

    250410 planetscale 디비 호스팅

--- admin/backend/utils/dbUtil.js
+++ admin/backend/utils/dbUtil.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 const db = require('mysql2');
 
 // 데이터베이스 연결 객체 생성
-const conn = db.createPool({
-    // host: 'localhost',
-    // user: 'iloke',
-    // password: '1q2w3e4r!@#',
-    // database: 'iloke'
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const conn = db.createPool(process.env.DATABASE_URL);
+
+conn.query('show tables', function (err, results, fields) {
+    console.log(results) // results contains rows returned by server
+    console.log(fields) // fields contains extra metadata about results, if available
+})
+
+conn.query('select 1 from dual where ? = ?', [1, 1], function (err, results) {
+    console.log(results)
+})
 
 // 서버 종료 시 DB 연결 종료
 process.on('SIGINT', () => {
